b6a2ad8c63c5b214afae19a4fd7c57749eeca779
etg-andersson <erik.andersson.7@gu.se>
2024-08-09 14:24:15 +0200

    test 2 uni

--- universities.py
+++ universities.py
@@ -10,6 +10,38 @@ def university_view():
     import io
     from pathlib import Path
 
+    # Construct file paths using Pathlib
+    file_path_university = 'affiliations_university_norm.csv'
+    file_path_university2 = 'affiliations_university_decoder_list.csv'
+    file_path_topic_codes = 'Topic_Codes.csv'
+
+    # Read the CSV files
+    try:
+        df_university = pd.read_csv(file_path_university)
+        df_university2 = pd.read_csv(file_path_university2)
+    except Exception as e:
+        st.error(f"Error loading files: {e}")
+
+    # Load university data
+    universities = pd.read_csv(file_path_university)
+    universities['Code'] = universities['Code'].astype(str)
+
+    # Load university decoder list data
+    universities2 = pd.read_csv(file_path_university2)
+    universities2['Code'] = universities2['Code'].astype(str)
+
+    #Import topic codes indexing
+    topic_codes_df = pd.read_csv(file_path_topic_codes, sep=';')
+    topic_codes_df['Code'] = topic_codes_df['Code'].astype(str)
+
+    # Extract unique major areas and specialties
+    major_areas = sorted(topic_codes_df[(topic_codes_df['Code'].str.len() == 3) & (topic_codes_df['Code'].str.startswith('3'))]['Swedish'])
+    specialties = sorted(topic_codes_df[(topic_codes_df['Code'].str.len() == 5) & (topic_codes_df['Code'].str.startswith('3'))]['Swedish'])
+
+    # Add "All" option to the dropdowns
+    major_areas.insert(0, "All")
+    specialties.insert(0, "All")
+
 
     # Function to fetch data from the database based on filters
     #### Tillfällig filter för titel istället för topic
